ddfc7cf987d167ef04f3f387dc51702883880df5
lalyos <lalyos@yahoo.com>
2016-04-04 16:15:26 -0700

    add custom color and body

--- start.sh
+++ start.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
-echo "${PRESIDENT} for president" > /usr/share/nginx/html/index.html
+
+cat > /usr/share/nginx/html/index.html <<EOF
+<html>
+<body bgcolor="${COLOR:=gray}">
+<h1>${PRESIDENT:=nobody} for president !</h1>
+${CUSTOM_BODY}
+</body>
+</html>
+EOF
+
 nginx -g "daemon off;"
